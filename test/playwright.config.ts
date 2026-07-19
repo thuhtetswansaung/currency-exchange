8826d5b71f3bb36c83780e5e3c522ba4d63fc177
Thu Htet Swan Saung <thuhtetswansaung@Thus-MacBook-Air.local>
2026-07-19 17:10:37 +0700

    debug

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -22,7 +22,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['list'],
+  ['html']
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,6 +34,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  timeout: 30000,
 
   /* Configure projects for major browsers */
   projects: [
